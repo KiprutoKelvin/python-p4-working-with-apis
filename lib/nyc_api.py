cb85c05154fb9d0d67c98acdd2e269872faa6723
iamkipruto <kelvin.kipruto@student.moringaschool.com>
2023-06-22 14:52:04 +0300

    Program obj

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -12,15 +12,18 @@ class GetPrograms:
   
   def program_school(self):
     # we use the JSON library to parse the API response into nicely formatted JSON
-    programs_list = []
-        programs = json.loads(self.get_programs())
-        for program in programs:
+      programs_list = []
+      programs = json.loads(self.get_programs())
+      for program in programs:
             programs_list.append(program["agency"])
 
-        return programs_list
+            return programs_list
 
 
 # programs = GetPrograms().get_programs()
 # print(programs)
-  for school in set(programs_schools):
+programs = GetPrograms()
+programs_schools = programs.program_school()
+
+for school in set(programs_schools):
       print(school)
